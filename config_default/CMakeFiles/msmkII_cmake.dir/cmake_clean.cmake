edb721b0f139f88ee92d525823dcddeed67d93ec
Diogo R Roessler <drroessler@hotmail.com.br>
2025-04-12 04:02:05 -0300

    Second

---
+++ config_default/CMakeFiles/msmkII_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msmkII_cmake.dir/Sources/gpio.c.obj"
+  "CMakeFiles/msmkII_cmake.dir/Sources/gpio.c.obj.d"
+  "CMakeFiles/msmkII_cmake.dir/Sources/main.c.obj"
+  "CMakeFiles/msmkII_cmake.dir/Sources/main.c.obj.d"
+  "CMakeFiles/msmkII_cmake.dir/Sources/spi.c.obj"
+  "CMakeFiles/msmkII_cmake.dir/Sources/spi.c.obj.d"
+  "CMakeFiles/msmkII_cmake.dir/Sources/syscalls.c.obj"
+  "CMakeFiles/msmkII_cmake.dir/Sources/syscalls.c.obj.d"
+  "CMakeFiles/msmkII_cmake.dir/Sources/sysmem.c.obj"
+  "CMakeFiles/msmkII_cmake.dir/Sources/sysmem.c.obj.d"
+  "CMakeFiles/msmkII_cmake.dir/Startup/startup_stm32f103c6tx.s.obj"
+  "libmsmkII_cmake.dll.a"
+  "msmkII_cmake.elf"
+  "msmkII_cmake.elf.manifest"
+  "msmkII_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/msmkII_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
